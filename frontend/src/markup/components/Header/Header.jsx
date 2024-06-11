fe82d2eb8c2687ea7e79ddb5b8579a4293adfaa6
Abelmekonn <Abelmekonn9@gmail.com>
2024-06-11 22:10:53 +0300

    "Updated Header component: replaced class with className, added alt text to images, and reformatted code."

--- frontend/src/markup/components/Header/Header.jsx
+++ frontend/src/markup/components/Header/Header.jsx
@@ -1,105 +1,96 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
     return (
         <div>
-            <header class="main-header header-style-one">
-                <div class="header-top">
-                    <div class="auto-container">
-                        <div class="inner-container">
-                            <div class="left-column">
-                                <div class="text">Enjoy the Beso while we fix your car</div>
-                                <div class="office-hour">Monday - Saturday 7:00AM - 6:00PM</div>
+            <header className="main-header header-style-one">
+                <div className="header-top">
+                    <div className="auto-container">
+                        <div className="inner-container">
+                            <div className="left-column">
+                                <div className="text">Enjoy the Beso while we fix your car</div>
+                                <div className="office-hour">Monday - Saturday 7:00AM - 6:00PM</div>
                             </div>
-                            <div class="right-column">
-                                <div class="phone-number">Schedule Your Appontment Today : <strong>1800 456 7890</strong>
+                            <div className="right-column">
+                                <div className="phone-number">
+                                    Schedule Your Appointment Today : <strong>1800 456 7890</strong>
                                 </div>
-
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="header-upper">
-                    <div class="auto-container">
-                        <div class="inner-container">
-                            <div class="logo-box">
-                                <div class="logo"><a href="/"><img src="assets/images/custom/logo.png" alt="" /></a>
+                <div className="header-upper">
+                    <div className="auto-container">
+                        <div className="inner-container">
+                            <div className="logo-box">
+                                <div className="logo">
+                                    <a href="/"><img src="assets/images/custom/logo.png" alt="Company Logo" /></a>
                                 </div>
                             </div>
-                            <div class="right-column">
-                                <div class="nav-outer">
-                                    
-                                    <div class="mobile-nav-toggler"><img src="assets/images/icons/icon-bar.png" alt="" />
+                            <div className="right-column">
+                                <div className="nav-outer">
+                                    <div className="mobile-nav-toggler">
+                                        <img src="assets/images/icons/icon-bar.png" alt="Menu Icon" />
                                     </div>
-                                    <nav class="main-menu navbar-expand-md navbar-light">
-                                        <div class="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
-                                            <ul class="navigation">
-                                                <li class="dropdown"><a href="/">Home</a>
-                                                </li>
-                                                <li class="dropdown"><a href="/about">About Us</a>
-                                                </li>
-                                                <li class="dropdown"><a href="/services">Services</a>
-                                                </li>
+                                    <nav className="main-menu navbar-expand-md navbar-light">
+                                        <div className="collapse navbar-collapse show clearfix" id="navbarSupportedContent">
+                                            <ul className="navigation">
+                                                <li className="dropdown"><a href="/">Home</a></li>
+                                                <li className="dropdown"><a href="/about">About Us</a></li>
+                                                <li className="dropdown"><a href="/services">Services</a></li>
                                                 <li><a href="/contact">Contact Us</a></li>
                                             </ul>
                                         </div>
                                     </nav>
                                 </div>
-                                <div class="search-btn"></div>
-                                <div class="link-btn"><a href="/login" class="theme-btn btn-style-one">Login</a></div>
+                                <div className="search-btn"></div>
+                                <div className="link-btn"><a href="/login" className="theme-btn btn-style-one">Login</a></div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="sticky-header">
-                    <div class="header-upper">
-                        <div class="auto-container">
-                            <div class="inner-container">
-                                <div class="logo-box">
-                                    <div class="logo"><a href="/"><img src="assets/images/custom/logo.png" alt="" /></a>
+                <div className="sticky-header">
+                    <div className="header-upper">
+                        <div className="auto-container">
+                            <div className="inner-container">
+                                <div className="logo-box">
+                                    <div className="logo">
+                                        <a href="/"><img src="assets/images/custom/logo.png" alt="Company Logo" /></a>
                                     </div>
                                 </div>
-                                <div class="right-column">
-                                    
-                                    <div class="nav-outer">
-                                        
-                                        <div class="mobile-nav-toggler"><img src="assets/images/icons/icon-bar.png" alt="" />
+                                <div className="right-column">
+                                    <div className="nav-outer">
+                                        <div className="mobile-nav-toggler">
+                                            <img src="assets/images/icons/icon-bar.png" alt="Menu Icon" />
                                         </div>
-
-                                        
-                                        <nav class="main-menu navbar-expand-md navbar-light">
+                                        <nav className="main-menu navbar-expand-md navbar-light">
+                                            {/* Empty nav can be filled as needed */}
                                         </nav>
                                     </div>
-                                    <div class="search-btn"></div>
-                                    <div class="link-btn"><a href="/login" class="theme-btn btn-style-one">Login</a>
-                                    </div>
+                                    <div className="search-btn"></div>
+                                    <div className="link-btn"><a href="/login" className="theme-btn btn-style-one">Login</a></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
                 </div>
-
-                
-                <div class="mobile-menu">
-                    <div class="menu-backdrop"></div>
-                    <div class="close-btn"><span class="icon flaticon-remove"></span></div>
-
-                    <nav class="menu-box">
-                        <div class="nav-logo"><a href="index.html"><img src="assets/images/logo-two.png" alt="" ></a></div>
-                        <div class="menu-outer">
+                <div className="mobile-menu">
+                    <div className="menu-backdrop"></div>
+                    <div className="close-btn"><span className="icon flaticon-remove"></span></div>
+                    <nav className="menu-box">
+                        <div className="nav-logo"><a href="/"><img src="assets/images/logo-two.png" alt="Mobile Logo" /></a></div>
+                        <div className="menu-outer">
+                            {/* Mobile menu items can be added here */}
                         </div>
-
                     </nav>
                 </div>
-
-                <div class="nav-overlay">
-                    <div class="cursor"></div>
-                    <div class="cursor-follower"></div>
+                <div className="nav-overlay">
+                    <div className="cursor"></div>
+                    <div className="cursor-follower"></div>
                 </div>
             </header>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
